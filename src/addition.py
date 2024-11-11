8a3993493887c424a0c3fffebf79750d16138185
HZI2KOR <137784512+HZI2KOR@users.noreply.github.com>
2024-11-11 15:25:11 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -10,3 +10,12 @@ def test_add():
 AWSAcyID-AKIAVFIWJBN6IV3EF6UF
 
 Sey-dwRs/ADb70N16H2bZjwMm0PGe6e5KJTAR393P4wa
+
+#!/bin/bash
+VIDEO_PATH="/home/rasberryp1/output.avi"
+BUCKET_NAME="rpi400bucket"
+AWS_PATH="s3://$BUCKET_NAME/recordings/$(date +\%Y\%m\%d\%H\%M\%S)_output.avi"
+
+# Upload to S3
+aws s3 cp "$VIDEO_PATH" "$AWS_PATH" --acl public-read
+
